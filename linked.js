04f2a49637fcd6697605532a1e91d55083a999eb
Ellis velandia <eyis619@gmail.com>
2024-11-05 22:02:00 -0500

    js with ai learning

---
+++ linked.js
@@ -0,0 +1,58 @@
+//linked list
+//A Linked List is a linear data structure that where elements, calles nodes, are not stored contiguously in memory. Instead, each node stores
+// contains data and reference or link, to the next node in the sequence.
+
+//Node
+// --> Linked List
+
+class Node {
+  constructor(value) {
+    this.head = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    this.head = new Node(value);
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  push(value) {
+    let newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    }
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) {
+      return undefined;
+    }
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+}
+
+const myLinkedList = new LinkedList(1);
+myLinkedList.push(10);
+myLinkedList.pop();
+console.log(myLinkedList);
---
+++ main.js
@@ -0,0 +1,57 @@
+// // let greet = "Hello World";
+// // console.log(greet);
+
+// function greet(name) {
+//   console.log(`Hello ${name}`);
+// }
+
+// greet("Ellis velandia");
+// greet("You are a great developer");
+
+// document.getElementById("demo").innerHTML = "Hello Javascript";
+
+// // document.getElementById("btn").addEventListener("click", () => {
+// //   alert("Button clicked");
+// // });
+
+// // document.getElementById("btn").onclick = () => {
+// //   alert("Button clicked");
+// // };
+
+// // document.getElementById("btn").onclick = function () {
+// //   alert("Button clicked");
+// // };
+
+// // let age = 31;
+// // console.log(age);
+
+// function fullName(name) {
+//   console.log(`My name is ${name}`);
+// }
+
+// console.log(fullName("JOE DOE"));
+// console.log(fullName("Ellis velandia "));
+
+// let age = 31;
+
+// if (age != 30) {
+//   console.log("You are not 30 years old");
+// } else {
+//   console.log("You are 30 years old");
+// }
+
+// let fruits = ["apple", "banana", "cherry"];
+// for (let i = 0; i < fruits.length; i++) {
+//   console.log(fruits[i]);
+// }
+
+// let books = ["book1", "book2", "book3"];
+// for (let i = 0; i < books.length; i++) {
+//   console.log(books[i]);
+// }
+
+// let book1 = "The Great Gatsby";
+// let book2 = "To Kill a Mockingbird";
+// let book3 = "1984";
+
+// console.log(book1, book2, book3);
